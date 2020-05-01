7f4842a1ba2a006da3f781bb1c6a75f20d738e90
lmasiello <lucas.masiello@agilecontent.com>
2020-05-01 15:10:02 -0300

    set connections

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-import express from 'express';
+import mongoose, { mongo } from 'mongoose'
+import express from 'express'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import Redis from 'ioredis'
+import { REDIS_OPTIONS, SESSION_OPTIONS, APP_PORT, MONGO_URI, MONGO_OPTIONS } from './config'
 
-const app = express();
+(async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+  const RedisStore = connectRedis(session)
 
-app.get('/', (req, res) => res.json({ message: 'HI!' }));
-app.listen(3000, () => console.log('http://localhost:3000'));
+  const client = new Redis(REDIS_OPTIONS)
+
+
+  const app = express()
+  app.use(
+    session({
+      ...SESSION_OPTIONS,
+      store: new RedisStore({ client })
+    })
+  )
+
+  app.get('/', (req, res) => res.json({ message: 'HI!' }))
+  app.listen(3000, () => console.log(`http://localhost${APP_PORT}`))
+})();
+
